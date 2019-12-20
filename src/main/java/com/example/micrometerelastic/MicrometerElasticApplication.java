625971483879d62b92ff476b662f1c72f6556521
test <test@corelogic.com>
2019-12-20 09:17:24 -0500

    initial commit

---
+++ src/main/java/com/example/micrometerelastic/MicrometerElasticApplication.java
@@ -0,0 +1,13 @@
+package com.example.micrometerelastic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrometerElasticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrometerElasticApplication.class, args);
+	}
+
+}
